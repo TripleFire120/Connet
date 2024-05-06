06350e697664d5cda513f07d709e62aa0f99720f
TripleFire <vitek.slama@gmail.com>
2024-05-06 18:53:02 +0200

    Initialize

---
+++ src/test/java/slama/Connet/ConnetApplicationTests.java
@@ -0,0 +1,13 @@
+package slama.Connet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
